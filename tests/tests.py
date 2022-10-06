a428035ceb7e7f5156eeb9e6924ed165fb5410bd
Olly Ewert <olly@fantail.co>
2022-10-06 14:27:09 +1300

    keep flake8 happy

--- tests/tests.py
+++ tests/tests.py
@@ -1,7 +1,6 @@
 import pytest
 from markdown_table import MarkdownTable
 
-
 bad_data_0 = []
 
 bad_data_1 = [{'one': 'two', 'three': 'four'},
@@ -42,48 +41,51 @@ def test_formatting_plain():
 
 
 def test_formatting_topbottom():
-    mt = MarkdownTable(formatting_data,row_sep='topbottom').get_markdown()
+    mt = MarkdownTable(formatting_data, row_sep='topbottom').get_markdown()
     res = '\n+----------------------------------------+\n|    title   |    time   |   date  |seats|\n|Vrij Zwemmen|21:30-23:00|Wed 09.12|24/24|\n|Vrij Zwemmen|12:00-13:00|Thu 10.12|18/18|\n|Vrij Zwemmen| 7:30-8:30 |Fri 11.12|18/18|\n|Vrij Zwemmen|13:15-14:15|Sat 12.12|18/18|\n+----------------------------------------+'
     assert mt == res
 
 
 def test_formatting_markdown():
-    mt = MarkdownTable(formatting_data,row_sep='markdown').get_markdown()
+    mt = MarkdownTable(formatting_data, row_sep='markdown').get_markdown()
     res = '|    title   |    time   |   date  |seats|\n|------------|-----------|---------|-----|\n|Vrij Zwemmen|21:30-23:00|Wed 09.12|24/24|\n|Vrij Zwemmen|12:00-13:00|Thu 10.12|18/18|\n|Vrij Zwemmen| 7:30-8:30 |Fri 11.12|18/18|\n|Vrij Zwemmen|13:15-14:15|Sat 12.12|18/18|'
     assert mt == res
 
 
 def test_formatting_markdown_noquote():
-    mt = MarkdownTable(formatting_data,row_sep='markdown').get_markdown()
+    mt = MarkdownTable(formatting_data, row_sep='markdown').get_markdown()
     res = '|    title   |    time   |   date  |seats|\n|------------|-----------|---------|-----|\n|Vrij Zwemmen|21:30-23:00|Wed 09.12|24/24|\n|Vrij Zwemmen|12:00-13:00|Thu 10.12|18/18|\n|Vrij Zwemmen| 7:30-8:30 |Fri 11.12|18/18|\n|Vrij Zwemmen|13:15-14:15|Sat 12.12|18/18|'
     assert mt == res
 
 
 def test_formatting_right():
-    mt = MarkdownTable(formatting_data,row_sep='topbottom', padding_weight='right').get_markdown()
+    mt = MarkdownTable(formatting_data, row_sep='topbottom', padding_weight='right').get_markdown()
     res = '\n+----------------------------------------+\n|title       |time       |date     |seats|\n|Vrij Zwemmen|21:30-23:00|Wed 09.12|24/24|\n|Vrij Zwemmen|12:00-13:00|Thu 10.12|18/18|\n|Vrij Zwemmen|7:30-8:30  |Fri 11.12|18/18|\n|Vrij Zwemmen|13:15-14:15|Sat 12.12|18/18|\n+----------------------------------------+'
     assert mt == res
 
 
 def test_formatting_padding1():
-    mt = MarkdownTable(formatting_data,row_sep='topbottom', padding_width=5, padding_weight='left').get_markdown()
+    mt = MarkdownTable(formatting_data, row_sep='topbottom', padding_width=5, padding_weight='left').get_markdown()
     res = '\n+------------------------------------------------------------+\n|            title|            time|          date|     seats|\n|     Vrij Zwemmen|     21:30-23:00|     Wed 09.12|     24/24|\n|     Vrij Zwemmen|     12:00-13:00|     Thu 10.12|     18/18|\n|     Vrij Zwemmen|       7:30-8:30|     Fri 11.12|     18/18|\n|     Vrij Zwemmen|     13:15-14:15|     Sat 12.12|     18/18|\n+------------------------------------------------------------+'
     assert mt == res
 
 
 def test_formatting_padding2():
-    mt = MarkdownTable(formatting_data,row_sep='topbottom', padding_width=5, padding_weight='centerright').get_markdown()
+    mt = MarkdownTable(formatting_data, row_sep='topbottom', padding_width=5,
+                       padding_weight='centerright').get_markdown()
     res = '\n+------------------------------------------------------------+\n|      title      |      time      |     date     |  seats   |\n|  Vrij Zwemmen   |  21:30-23:00   |  Wed 09.12   |  24/24   |\n|  Vrij Zwemmen   |  12:00-13:00   |  Thu 10.12   |  18/18   |\n|  Vrij Zwemmen   |   7:30-8:30    |  Fri 11.12   |  18/18   |\n|  Vrij Zwemmen   |  13:15-14:15   |  Sat 12.12   |  18/18   |\n+------------------------------------------------------------+'
     assert mt == res
 
 
 def test_formatting_padding_char():
-    mt = MarkdownTable(formatting_data,row_sep='always', padding_width=5, padding_weight='centerright', padding_char='.').get_markdown()
+    mt = MarkdownTable(formatting_data, row_sep='always', padding_width=5, padding_weight='centerright',
+                       padding_char='.').get_markdown()
     res = '\n+------------------------------------------------------------+\n|......title......|......time......|.....date.....|..seats...|\n+-----------------+----------------+--------------+----------+\n|..Vrij Zwemmen...|..21:30-23:00...|..Wed 09.12...|..24/24...|\n+-----------------+----------------+--------------+----------+\n|..Vrij Zwemmen...|..12:00-13:00...|..Thu 10.12...|..18/18...|\n+-----------------+----------------+--------------+----------+\n|..Vrij Zwemmen...|...7:30-8:30....|..Fri 11.12...|..18/18...|\n+-----------------+----------------+--------------+----------+\n|..Vrij Zwemmen...|..13:15-14:15...|..Sat 12.12...|..18/18...|\n+------------------------------------------------------------+'
     assert mt == res
 
 
 def test_multiline_data():
-    mt = MarkdownTable(multiline_data,padding_width=2, padding_weight="centerleft", multiline={"A": 25, "B": 12, "C": 9}).get_markdown()
+    mt = MarkdownTable(multiline_data, padding_width=2, padding_weight="centerleft",
+                       multiline={"A": 25, "B": 12, "C": 9}).get_markdown()
     res = '\n+------------------------------------------------+\n|            A            |      B     |    C    |\n+-------------------------+------------+---------+\n|  row1_A and additional  |   row1_B   |  row1_C |\n|          stuff          |            |         |\n+-------------------------+------------+---------+\n|          row2_A         | row2_B and |  row2_C |\n|                         | additional |         |\n|                         |    stuff   |         |\n+-------------------------+------------+---------+\n|          row3_A         |   row3_B   |  row3_C |\n+------------------------------------------------+'
     assert mt == res
